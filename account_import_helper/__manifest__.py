3256232aa93782c22b86dd1c89825135a0063298
Alexis de Lattre <alexis.delattre@akretion.com>
2016-11-08 21:21:51 +0100

    Add module account_import_helper

---
+++ account_import_helper/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Import Helper',
+    'version': '10.0.1.0.0',
+    'category': 'Partner',
+    'license': 'AGPL-3',
+    'summary': 'Helper methods to import accounting-related data',
+    'description': """
+Account Import Helper
+=====================
+
+This module provides methods to help on the import of accounting-related data, in particular the chart of accounts.
+
+This module has been written by Alexis de Lattre <alexis.delattre@akretion.com> from Akretion.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['account'],
+    'installable': True,
+}
---
+++ account_import_helper/account.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api, _
+from openerp.exceptions import UserError
+import logging
+import unicodecsv
+logger = logging.getLogger(__name__)
+
+OUTFILE = '/tmp/account.account.csv'
+
+
+class AccountChartTemplate(models.Model):
+    _inherit = 'account.chart.template'
+
+    @api.model
+    def generate_id2xmlid(self, object_name):
+        irmdo = self.env['ir.model.data']
+        obj_id2xmlid = {}
+        irmd = irmdo.search([
+            ('model', '=', object_name),
+            ('res_id', '!=', False)])
+        for entry in irmd:
+            obj_id2xmlid[entry.res_id] = '%s.%s' % (entry.module, entry.name)
+        return obj_id2xmlid
+
+    @api.multi
+    def generate_custom_chart(
+            self, custom_chart, module, account_xmlid_prefix):
+        self.ensure_one()
+        aato = self.env['account.account.template']
+        user_type_id2xmlid = self.generate_id2xmlid('account.account.type')
+        taxtemplate2xmlid = self.generate_id2xmlid('account.tax.template')
+        logger.info('taxtemplate2xmlid = %s', taxtemplate2xmlid)
+        logger.info('user_type_id2xmlid = %s', user_type_id2xmlid)
+        odoo_chart = {}
+        accounts = aato.search([('chart_template_id', '=', self.id)])
+        odoo_code_size = False
+        for account in accounts:
+            user_type_xmlid = user_type_id2xmlid[account.user_type_id.id]
+            taxes_xmlids = [
+                taxtemplate2xmlid[tax.id] for tax in account.tax_ids]
+            odoo_chart[account.code] = {
+                'name': account.name,
+                'reconcile': account.reconcile,
+                'user_type_xmlid': user_type_xmlid,
+                'tax_xmlids': ','.join(taxes_xmlids),
+                }
+            if not odoo_code_size:
+                odoo_code_size = len(account.code)
+        res = []
+        # header line
+        res.append({
+            'id': 'id',
+            'code': 'code',
+            'name': 'name',
+            'user_type_xmlid': 'user_type_id/id',
+            'tax_xmlids': 'tax_ids/id',
+            'reconcile': 'reconcile',
+            'note': 'note',
+        })
+        custom_code_size = False
+        for (custom_code, src_custom_dict) in custom_chart:
+            if custom_code_size:
+                assert len(custom_code) == custom_code_size, 'wrong size'
+            else:
+                custom_code_size = len(custom_code)
+                assert custom_code_size >= odoo_code_size,\
+                    'custom size can only be bigger than odoo size'
+            size = odoo_code_size
+            match = False
+            print "custom_code=", custom_code
+            while size > 1 and not match:
+                short_custom_code = custom_code[:size]
+                for odoo_code, odoo_dict in odoo_chart.iteritems():
+                    if odoo_code.startswith(short_custom_code):
+                        custom_dict = odoo_dict.copy()
+                        custom_dict['id'] = '%s.%s%s' % (
+                            module, account_xmlid_prefix, custom_code)
+                        custom_dict.update(src_custom_dict)
+                        custom_dict['code'] = custom_code
+                        res.append(custom_dict)
+                        match = True
+                        break
+                size -= 1
+            if not match:
+                raise UserError(_(
+                    "Customer account %s didn't match any Odoo account")
+                    % custom_code)
+
+        # generate file
+        f = open(OUTFILE, 'w')  # it will over-write an existing file
+        w = unicodecsv.DictWriter(f, [
+            'id',
+            'code',
+            'name',
+            'user_type_xmlid',
+            'reconcile',
+            'tax_xmlids',
+            'note',
+            ], encoding='utf-8')
+        for account_dict in res:
+            w.writerow(account_dict)
+        f.close()
+        return OUTFILE
+
+    @api.model
+    def generate_l10n_fr_custom(self, custom_fr_pcg):
+        # This is a sample method
+        # custom_fr_pcg is a list of tuple:
+        # (code, {'name': 'Déplacement', 'note': 'My comment'}:
+        fr_pcg = self.env.ref('l10n_fr.l10n_fr_pcg_chart_template')
+        company = self.env.user.company_id
+        if company.chart_template_id != fr_pcg:
+            raise UserError(_(
+                'The chart of accounts of the company %s is not the chart of '
+                'account of the official Odoo module l10n_fr') % company.name)
+        assert isinstance(custom_fr_pcg, list), 'custom_fr_pcg must be a dict'
+        for (code, acc_dict) in custom_fr_pcg:
+            assert len(code) > 3, 'account code is too small ?'
+            assert isinstance(acc_dict.get('name'), (str, unicode)),\
+                'missing account name'
+            assert code.isdigit(), 'code should only have digits'
+        company.chart_template_id.generate_custom_chart(
+            custom_fr_pcg, 'emaj_custom', 'pcg_')
