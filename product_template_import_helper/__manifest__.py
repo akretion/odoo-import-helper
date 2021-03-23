5288d46058dc2a9c6a4260a70698c842312469c7
Sébastien BEAU <sebastien.beau@akretion.com>
2021-03-23 11:23:42 +0100

    [MIG] product_template_import_helper: Migration to 14.0

--- product_template_import_helper/__manifest__.py
+++ product_template_import_helper/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Product Template Import Helper",
     "summary": "Helper for importing product template",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Import",
     "website": "www.akretion.com",
     "author": " Akretion",
