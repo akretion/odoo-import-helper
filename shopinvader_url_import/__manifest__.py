3fbaabb229a719d08aa243356a2b7e4f458ad6ee
Sébastien BEAU <sebastien.beau@akretion.com>
2022-10-27 17:07:15 +0200

    add shopinvader_url_import

---
+++ shopinvader_url_import/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2022 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Shopinvader URL Import",
+    "summary": "Allow to import url in shopinvader",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Uncategorized",
+    "website": "https://github.com/akretion/odoo-import-helper",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "shopinvader",
+    ],
+    "data": [],
+    "demo": [],
+}
