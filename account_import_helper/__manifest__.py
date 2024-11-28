d9b970808e7611eaa3bf7d2de493e7429919183f
Florian da Costa <florian.dacosta@akretion.com>
2024-11-28 09:10:26 +0100

    [MIG] Migrate account_import_helper to version 18

--- account_import_helper/__manifest__.py
+++ account_import_helper/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Import Helper",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Partner",
     "license": "AGPL-3",
     "summary": "Helper methods to import accounting-related data",
@@ -16,5 +16,5 @@
         "security/ir.model.access.csv",
         "wizard/account_chart_generate_view.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
